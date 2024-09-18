5694f0f1f309a2583aae47e293e4cc2b28d3d13e
RelAthalla <farrelathalla0910@gmail.com>
2024-09-18 11:44:50 +0700

    readme update and bugfix

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ import uuid
 
 # Create your models here.
 class ProductEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
