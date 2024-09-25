57b8609760aea864bbe1807fb966892b18251e7d
RelAthalla <farrelathalla0910@gmail.com>
2024-09-25 11:49:44 +0700

    tugas 4

--- muljawan_store/settings.py
+++ muljawan_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l4@e58z(g%cn*hczp@$gw-n+!ap#mkv#wh%kzxg2#^@-wmv19e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","farrel-athalla-muljawanstoredua.pbp.cs.ui.ac.id",
                  "farrel-athalla-muljawanstore.pbp.cs.ui.ac.id"]
